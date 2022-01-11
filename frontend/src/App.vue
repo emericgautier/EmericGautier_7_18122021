ba0d9ddac8536b6c3cf156bf4b4c99e8eb14987c
emericgautier <emericgtr@gmail.com>
2022-01-11 16:44:55 +0100

    import and config Vue Router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+
+    <router-view />
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+
+
+  export default {
+
+  }
+
+</script>
+
+<style lang="scss">
+ 
+  html, body, #app{
+    height: 100%;
+  }
+
+  // app
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  // sign-in
+
+  .form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+  }
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+        text-decoration: none;
+        color: white;
     }
+
+  .btn-danger {
+    background-color: #fd2d01;
   }
-}
+
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,67 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Landingpage from '@/views/Landingpage.vue'
+import Groupomania from '@/views/Groupomania.vue'
+
+import Home from '@/components/home/Home.vue'
+import Profile from '@/components/profile/Profile.vue'
+import Posts from '@/components/post/Post.vue'
+import Login from '@/components/landingpage/Login.vue'
+import Register from '@/components/landingpage/Register.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+    path: '/', 
+    component: Landingpage, 
+    children: [
+    {
+      path: '/', component: Login
+    },
+    {
+      path: '/register', component: Register
+    }
+  ]
+},
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/home', 
+    component: Groupomania,
+    name: 'home',
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: '/home', component: Home, name: 'home'
+      },
+      {
+        path: '/profile/:username', name: 'profile', component: Profile, props: true
+      },
+      {
+        path: '/post/:id', name: 'post', component: Posts, props: true
+      }
+    ]
+  },
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach( async (to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    const user = await fetch('http://localhost:3000/' + 'api/user/auth', {
+      headers: {'Content-Type': 'application/json'},
+      credentials: 'include'
+    })
+    if (user.status != 200) {
+      next({
+        path: '/',
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() 
+  }
 })
 
 export default router
